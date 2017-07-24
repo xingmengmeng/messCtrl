62f27ee63fb4377fda463ee81b2f6bf18ad713e9
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-07-24 14:42:50 +0800

    自助分析平台逻辑修改

--- src/views/selfHelp/dataFactory.vue
+++ src/views/selfHelp/dataFactory.vue
@@ -13,11 +13,11 @@
                 </ul>
                 <!--数据库源信息 start-->
                 <div v-show="showDataIndex==1" class="botDiv clearfix">
-                    <select name="messSelect" id="messSelect" v-model="selected" @change="selectVal('5',selected)">
+                    <select name="messSelect" id="messSelect" v-model="selected" @change="selectVal('3',selected)">
                         <option value="">选择业务线</option>
                         <option :value="op.ocMenubm" v-for="op in oneLevelMenus" :key="op">{{op.vcMenumc}}</option>
                     </select>
-                    <select name="dataLibrary" id="dataLibrary" v-model="selectedSecond" @change="selectVal('6',selectedSecond)">
+                    <select name="dataLibrary" id="dataLibrary" v-model="selectedSecond" @change="selectVal('4',selectedSecond)">
                         <option value="">选择数据库</option>
                         <option :value="op.ocMenubm" v-for="op in twoLevelMenus" :key="op">{{op.vcMenumc}}</option>
                     </select>
@@ -250,9 +250,9 @@ export default {
         },
         /*联动菜单选择*/
         selectVal(level,menuNm){
-            if(level=='4'){
+            if(level=='3'){
                 this.menuShowSecond=menuNm;
-            }else if(level=='5'){
+            }else if(level=='4'){
                 this.twoLevelMenus.forEach((item) =>{
                     if(item.ocMenubm==menuNm){
                         this.menuShowSecond=item.ocParentbm;
@@ -262,11 +262,11 @@ export default {
             }
             this.$http.get('biPc/selfAnaPlatform/menusLink.gm?level='+level+'&ocMenubm='+menuNm).then(function(res){
                 if(res.data.code=='200'){
-                    if(level=='4'){
+                    if(level=='3'){
                         this.twoLevelMenus=res.data.data.dataInfo.twoLevelMenus;
                         this.threeLevelMenus=res.data.data.dataInfo.threeLevelMenus;
                         this.selectedSecond=this.selectedThree='';
-                    }else if(level=='5'){
+                    }else if(level=='4'){
                         this.threeLevelMenus=res.data.data.dataInfo.threeLevelMenus;
                         this.selectedThree='';
                     }
