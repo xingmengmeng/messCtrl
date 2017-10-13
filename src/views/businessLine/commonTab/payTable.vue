3959db5adf836a8dae7377db8af87113bd56ebd7
dongxingrui06 <dongxingrui@gomeholdings.com>
2017-10-13 19:34:13 +0800

    三方支付页面功能基本实现，页面选择维度的默认显示信息已联调

--- src/views/businessLine/commonTab/payTable.vue
+++ src/views/businessLine/commonTab/payTable.vue
@@ -20,23 +20,22 @@
         		<div class="sel">
         			<label class="companyp">业务主体：</label>
 	                <select class="company" >
-	                    <option value="1">1</option>
-	                    <option value="2">2</option>
-	                    <option value="3">3</option>
-	                    <option value="4">4</option>
-	                    <option value="5">5</option>
+	                    <option :value="items.value" v-if="defaultMes.incomeModelCom" v-for="items in defaultMes.incomeModelCom">{{items.text}}</option>
 	                </select>
         		</div>
         		<div class="dateChoose">
         			<span>完成时间：</span>
         			<ul class="btns">
-        				<li>日</li>
-        				<li>月</li>
-        				<li>年</li>
-        				<li>区间</li>
+        				<li :class="initDate=='日'?'active':''" data-value="日" @click="changeClass($event)">日</li>
+        				<li :class="initDate=='月'?'active':''" data-value="月" @click="changeClass($event)">月</li>
+        				<li :class="initDate=='年'?'active':''" data-value="年" @click="changeClass($event)">年</li>
+        				<li :class="initDate=='区间'?'active':''" data-value="区间" @click="changeClass($event)">区间</li>
         			</ul>
         			<div class="calendar">
-        				<input type="text" v-model="startTimes" class="smallTxt" readonly="" id="aboutDay"/>
+        				<input v-show="initDate == '日'" type="text" v-model="startDay" class="smallTxt" readonly id="aboutDay"/>
+        				<input v-show="initDate == '月'" type="text" v-model="startMonth" class="smallTxt" readonly id="aboutMonth"/>
+        				<input v-show="initDate == '年'" type="text" v-model="startYear" class="smallTxt" readonly id="aboutYear"/>
+        				<input v-show="initDate == '区间'" type="text" v-model="startRange" class="smallTxt" readonly id="aboutRange"/>
         			</div>
         		</div>
         		<div class="intoPieces">
@@ -82,42 +81,77 @@
 	</div>
 </template>
 <script type="text/ecmascript-6">
+	import * as laydate from '../../../assets/laydate/laydate.js'; //日历插件
 	require('../../../assets/css/payTable.less');//三方支付样式
 	require('../../../assets/css/layout.less');//全局样式
 	require('../../../assets/laydate/theme/default/laydate.css');//日历样式
 	
 	import pages from '../../../components/pages.vue';//分页组件
 	import setLeftHeight from '../../../assets/js/setLeftHeight';//设置左边栏的高度
-	import * as laydate from '../../../assets/laydate/laydate.js'; //日历插件
+	
 	
 	export default {
 		data(){
 			return{
-				startTimes:'2017-10-12'
+				startDay: '',//日历默认显示日期		日
+				startMonth: '',//日历默认显示日期		月
+				startYear: '',//日历默认显示日期		年
+				startRange: '',//日历默认显示日期		区间
+				defaultMes: '',//初始加载信息
+				initDate: '日'
 			}
 		},
 		mounted(){
-			let aboutDay = laydate.render({
-				elem: '#aboutDay',
-				type:'dateTime',
-				theme: 'lightblue',
-				value: '2017-10-12'
-			})
+			this.showCalendar();//日历默认显示
+			this.getData();//获取默认信息
 		},
 		components:{
             'pages':pages
         },
 		methods:{
-			//得到默认信息
+			//获取默认信息	包括日、月、年、区间的默认值	以及	业务主体下拉框信息
             getData(){
-                this.$http.get('biPc/debtChange/getBaseInfo.gm').then(function (res) {
+                this.$http.get('biPc/payChannel/init.gm').then(function (res) {
                     if(res.data.code==203){
                         window.location.href='login.html';
                         return;
                     }
-                    this.defaultMes=res.data.data.dataInfo;
-                    this.getAdminFlag();
+					this.defaultMes = res.data.data.dataInfo;
+					this.startDay = this.defaultMes.day;
+					this.startMonth = this.defaultMes.month;
+					this.startYear = this.defaultMes.year;
+					this.startRange = this.defaultMes.section.replace(',',' - ');
                 });
+            },
+            showCalendar(){
+            	//日历插件默认显示
+            	let aboutDay = laydate.render({
+					elem: '#aboutDay',
+					theme: '#03BBFF',
+					value: this.startDay
+				})
+				let aboutMonth = laydate.render({
+					elem: '#aboutMonth',
+					theme: '#03BBFF',
+					type: 'month',
+					value: this.startMonth
+				})
+				let aboutYear = laydate.render({
+					elem: '#aboutYear',
+					theme: '#03BBFF',
+					type: 'year',
+					value: this.startYear
+				})
+				let aboutRange = laydate.render({
+					elem: '#aboutRange',
+					theme: '#03BBFF',
+					range: true,
+					value: this.startYear
+				})
+            },
+            changeClass(event){
+            	var curValue = event.target.getAttribute("data-value");
+            	this.initDate = curValue;
             }
 		}
 	}
