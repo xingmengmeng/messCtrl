bbd67e9e399e1ceadf51ac9863f701dd264a1dec
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-10-19 19:04:04 +0800

    整体结构变化

--- src/assets/js/setLeftHeight.js
+++ src/assets/js/setLeftHeight.js
@@ -1,11 +1,16 @@
 var setLeftHeight=(function () {
     return {
         init(){
-            var leftSide=document.querySelector('.leftSide'),
+            /*var leftSide=document.querySelector('.leftSide'),
                 rightSide=document.querySelector('.rightSide'),
                 rightHeight=rightSide.offsetHeight;
             var minHeight=document.documentElement.offsetHeight-70;
-            leftSide.style.height=minHeight>rightHeight?minHeight+'px':rightHeight+'px';
+            leftSide.style.height=minHeight>rightHeight?minHeight+'px':rightHeight+'px';*/
+            var leftSide=document.querySelector('.leftSide'),
+                rightSide=document.querySelector('.rightSide');
+            var minHeight=document.documentElement.offsetHeight-62;
+            leftSide.style.height=minHeight+'px';
+            rightSide.style.height=minHeight+'px';
         }
     }
 })();
